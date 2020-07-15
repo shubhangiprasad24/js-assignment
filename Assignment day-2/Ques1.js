ad44562729ba7e62c2da37ab28a55fc8485c29e6
shubhangiprasad24 <68220023+shubhangiprasad24@users.noreply.github.com>
2020-07-15 23:18:53 +0530

    Assignment Day - 2

---
+++ Assignment day-2/Ques1.js
@@ -0,0 +1,9 @@
+// Prompt
+
+let name = prompt("What is your name ?");
+
+console.log(name);
+
+let age = prompt("What is your age ?");
+
+console.log(age);
---
+++ Assignment day-2/Ques2.js
@@ -0,0 +1,19 @@
+let arr = ['one','two', 'three', ['1', '2']];
+console.log(arr);
+
+let arr2 = ['apple' , 'bananan','mango'];
+
+console.log(arr.concat(arr2));//	Joins two or more arrays, and returns a copy of the joined arrays
+
+console.log(arr2.pop());//	Removes the last element of an array, and returns that element
+
+console.log(arr.join()); // Joins all elements of an array into a string
+
+console.log(Array.isArray(arr)); // Checks whether an object is an array
+
+console.log(arr.lastIndexOf()); // Search the array for an element, starting at the end, and returns its position
+
+console.log(arr.sort()); // Sorts the elements of an array
+
+console.log(arr2.reverse()); // Reverses the order of the elements in an array
+ 
